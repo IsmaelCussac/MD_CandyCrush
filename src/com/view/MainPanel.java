3a800b7bf3f825a49d80fe11b15e7a272b5ccece
ismael <ismael.cussac@gmail.com>
2015-12-18 14:25:08 +0100

    travis

--- src/com/view/MainPanel.java
+++ src/com/view/MainPanel.java
@@ -1,17 +1,25 @@
 package com.view;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class MainPanel extends JPanel {
 	
 	public MainPanel() {
 		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
 		JPanel panel = new JPanel();
 		
+		tabbedPane.addTab("Menu", null, panel, null);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
 		JButton btnJouer = new JButton("Jouer"); 
 		btnJouer.addActionListener(new ActionListener() {
